d37fbc46cb4c99ff820ec6844913ab325ef4cbd7
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-03-09 19:46:45 +0530

    New triggers + comments + formatting changes

--- my_bot.js
+++ my_bot.js
@@ -1,5 +1,12 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// the trigger array triggers a response from the robot. The reply array is the resulting reply triggered. 
+// each message from the client is edited to match the items in the trigger array.
+// every sub-array at an index in the trigger array corresponds to the sub-array of possible replies in the reply array.
+/* For eg.: the first sub-array (index = 0) of trigger array would trigger/result in response 
+   from the first (index = 0) sub-array from the reply array. */
+
 const trigger = [
     //0 Greeting 
     ["hi", "hey", "hello","good morning","good evening", "good afternoon","hey there"],
@@ -55,10 +62,12 @@ const reply = [
     //3
     ["Glad to hear it", "That's great to hear"],
     //4
-    ["Why?", "Cheer up buddy", "What's the matter?", "Minor setbacks before a major comeback"],
+    ["Why?", "Cheer up buddy", "What's the matter?", "Minor setbacks before a major comeback", "Sometimes, life gives you lemons..."],
     //5
     ["A pig without 3.14 is 9.8", 
-     "What did the ghost say to the other ghost?...Do you believe in people?"
+     "What did the ghost say to the other ghost?...Do you believe in people?",
+     "Why did the nurse take a red pen to work?...In case she needed to draw blood",
+     "What do you call a psychic little person who has escaped the prison?...A smalle medium at large"
     ],
     //6
     ["You're welcome", "No problem", "Don't mention it", "Glad I could help", "It's my pleasure"],
@@ -67,13 +76,13 @@ const reply = [
     ["Goodbye", "See you later", "See you around"],
     
     //h1
-    ["Hi there, I am doctor Hannibal Lecter. How can I help you today?],
+    ["Hi there, I am Dr.Hannibal Lecter. How can I help you today?"],
     //h2
-    ["I have some free time right now, what should we talk about?],
+    ["I have some free time right now, what should we talk about?"],
     //h3
     ["I am sorry you feel that way, would you like some resources to assist you?", "I am sorry you feel that way, would you like to talk about it?"],
     //h4
-    ["Would you like to provide some more detail so I can assist you?]
+    ["Would you like to provide some more detail so I can assist you?"]
     //h5- quote
     ["I Do Wish We Could Chat Longer, But I'm Having An Old Friend For Dinner."],
      //h6- quote
@@ -84,7 +93,8 @@ const reply = [
     
       
 ];
-    
+
+// array used in case a match isn't found in the trigger array.
 const alternative = [
       "Same",
       "Go on...",
@@ -113,14 +123,14 @@ client.on('ready', () => {
         }
         receivedMessage.channel.send(output(receivedMessage.content));
     })
-})
+});
 
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "ODEyMTY4NjAwNjg0MDY4OTQ0.YC808w.zgRv4uEZdrEXWQDwvw3DJ8zABhs"
+bot_secret_token = "ODEyMTY4NjAwNjg0MDY4OTQ0.YC808w.zgRv4uEZdrEXWQDwvw3DJ8zABhs";
 
-client.login(bot_secret_token)
+client.login(bot_secret_token);
 
 
 function compare(triggerArray, replyArray, text) {
@@ -128,8 +138,8 @@ function compare(triggerArray, replyArray, text) {
     for (let x = 0; x < triggerArray.length; x++) {
       for (let y = 0; y < replyArray.length; y++) {
         if (text.includes(triggerArray[x][y])) {
-          items = replyArray[x];
-          item = item + " " + items[Math.floor(Math.random() * items.length)];
+          items = replyArray[x];                  // returns an array of possible replies
+          item = item + " " + items[Math.floor(Math.random() * items.length)];   // generates a random reply from the array
         }
       }
     }
@@ -138,16 +148,16 @@ function compare(triggerArray, replyArray, text) {
   function output(input) {
     let product;
     let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
-    text = text
+    text = text                   // function-chaining to convert the text to match items in the "trigger" array
       .replace(/ a /g, " ")
       .replace(/i feel /g, "")
       .replace(/whats/g, "what is")
       .replace(/please /g, "")
       .replace(/ please/g, "");
   
-  //compare arrays
-  //then search keyword
-  //then random alternative
+    //compare arrays
+    //then search keyword
+    //then random alternative
   
     if (compare(trigger, reply, text)) {
       product = compare(trigger, reply, text);
